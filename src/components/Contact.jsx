0c1ca524925710c88a941b77cc2ef6011a882fea
Nishikant sahoo <nishikantsahoo008@gmail.com>
2024-02-12 04:04:24 +0530

    firsst commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -61,7 +61,7 @@ function Contact() {
       <h6 className='g1'>Get In Touch</h6>
       <h3 className='g2'>Contact Me</h3>
       <p className='g3'></p>
-      <form onSubmit={submit} data-aos="fade-up" action='https://formspree.io/f/xeqyabvd' method='POST'>
+      <form onSubmit={submit} data-aos="fade-up" >
        
         <input type="text" name="name" id="g4" placeholder=' Your Name'  value={formData.name}
           onChange={handleChange} required/>
