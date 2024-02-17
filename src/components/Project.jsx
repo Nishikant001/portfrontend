c834e79a116a5ab8558211f3dac9a7709130ede7
Nishikant sahoo <nishikantsahoo008@gmail.com>
2024-02-17 15:46:27 +0530

    first commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./project.css";
-import meme from './Images/meme.png'
-import Love from './Images/love.png'
-import Story from './Images/story.png'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import meme from "./Images/meme.png";
+import Love from "./Images/love.png";
+import gym from "./Images/gym.png";
+import Story from "./Images/story.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function Project() {
-  useEffect(()=>{
-    AOS.init({
-      duration:2000
-    },[])
-  })
+  useEffect(() => {
+    AOS.init(
+      {
+        duration: 2000,
+      },
+      []
+    );
+  });
   return (
     <div className="f" id="projects" data-aos="fade-down">
       <h6 className="f1">Explore My</h6>
@@ -44,19 +48,65 @@ function Project() {
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <div class="card mb-3" >
+            <div class="card mb-3">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src={gym} class="img-fluid rounded-start" alt="..." />
+                  <a
+                    href="https://monstergym63.vercel.app/"
+                    target="_blank"
+                    className="f4 cc1"
+                  >
+                    View
+                  </a>
+                  <br />
+                  <a
+                    href="https://github.com/Nishikant001/GymFrontend"
+                    className="f4 cc"
+                  >
+                    | Resource
+                  </a>
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Monster Gym Web App</h5>
+                    <p class="card-text">
+                      Passionate Full Stack Developer with expertise in MERN
+                      (MongoDB, Express.js, React.js, Node.js) stack
+                      development. This dynamic Gym website offering seamless
+                      exploration, user signup, login, and contact functionality
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="card mb-3">
               <div class="row g-0">
                 <div class="col-md-4">
                   <img src={meme} class="img-fluid rounded-start" alt="..." />
-                  <a href="https://659d22b7c836b2930652fbb3--nimble-kashata-9b6d65.netlify.app/" target="_blank" className="f4 cc1">View</a> <br />
-                  <a href='https://app.netlify.com/teams/nishikant001/overview' className="f4 cc">|  Resource</a>
+                  <a
+                    href="https://659d22b7c836b2930652fbb3--nimble-kashata-9b6d65.netlify.app/"
+                    target="_blank"
+                    className="f4 cc1"
+                  >
+                    View
+                  </a>
+                  <br />
+                  <a
+                    href="https://app.netlify.com/teams/nishikant001/overview"
+                    className="f4 cc"
+                  >
+                    | Resource
+                  </a>
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">Meme Generator</h5>
                     <p class="card-text">
-                    Welcome to MemeMagic Reacto, the cutting-edge meme generator designed to amplify your content strategy!
-
+                      Welcome to MemeMagic Reacto, the cutting-edge meme
+                      generator designed to amplify your content strategy!
                     </p>
                     <p class="card-text">
                       <small class="text-body-secondary">
@@ -69,18 +119,30 @@ function Project() {
             </div>
           </div>
           <div class="carousel-item">
-          <div class="card mb-3" >
+            <div class="card mb-3">
               <div class="row g-0">
                 <div class="col-md-4">
-                <img src={Love} class="img-fluid rounded-start" alt="..." />
-                  <a href="https://nishikant001.github.io/Love-Calculator/" target="_blank" className="f4 cc1">View</a> <br />
-                  <a href='https://github.com/dashboard' className="f4 cc">|  Resource</a>
+                  <img src={Love} class="img-fluid rounded-start" alt="..." />
+                  <a
+                    href="https://nishikant001.github.io/Love-Calculator/"
+                    target="_blank"
+                    className="f4 cc1"
+                  >
+                    View
+                  </a>
+                  <br />
+                  <a href="https://github.com/dashboard" className="f4 cc">
+                    | Resource
+                  </a>
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">Love Predictor</h5>
                     <p class="card-text">
-                    The Love Percentage Predictor is an interactive web application designed to provide users with a fun and engaging way to predict the perceived level of affection between two individuals
+                      The Love Percentage Predictor is an interactive web
+                      application designed to provide users with a fun and
+                      engaging way to predict the perceived level of affection
+                      between two individuals
                     </p>
                     <p class="card-text">
                       <small class="text-body-secondary">
@@ -93,18 +155,29 @@ function Project() {
             </div>
           </div>
           <div class="carousel-item">
-          <div class="card mb-3" >
+            <div class="card mb-3">
               <div class="row g-0">
                 <div class="col-md-4">
-                <img src={Story} class="img-fluid rounded-start" alt="..." />
-                  <a href="https://nishikant001.github.io/ParallaX-StoryBook/" target="_blank" className="f4 cc1">View</a> <br />
-                  <a href='https://github.com/dashboard' className="f4 cc">|  Resource</a>
+                  <img src={Story} class="img-fluid rounded-start" alt="..." />
+                  <a
+                    href="https://nishikant001.github.io/ParallaX-StoryBook/"
+                    target="_blank"
+                    className="f4 cc1"
+                  >
+                    View
+                  </a>
+                  <br />
+                  <a href="https://github.com/dashboard" className="f4 cc">
+                    | Resource
+                  </a>
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">Parallax Story Book</h5>
                     <p class="card-text">
-                    The Parallax Story Book is a captivating web-based storytelling experience that employs parallax scrolling effects to animate and bring narratives to life.
+                      The Parallax Story Book is a captivating web-based
+                      storytelling experience that employs parallax scrolling
+                      effects to animate and bring narratives to life.
                     </p>
                     <p class="card-text">
                       <small class="text-body-secondary">
